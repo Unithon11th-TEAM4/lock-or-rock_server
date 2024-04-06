daaaca1f3c18918a6bbe63fe190bc6bc8079d8c1
ningpop <craft1933@naver.com>
2024-04-07 06:15:36 +0900

    fix : add validation logic

--- src/main/java/com/callmepeace/lockorrock/modules/quiz/api/dto/AnswerRequestDto.java
+++ src/main/java/com/callmepeace/lockorrock/modules/quiz/api/dto/AnswerRequestDto.java
@@ -3,15 +3,16 @@ package com.callmepeace.lockorrock.modules.quiz.api.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AnswerRequestDto {
 
-    @NotEmpty
+    @NotNull
     private Long questionId;
 
-    @NotEmpty
+    @NotNull
     private Long answerId;
 }
