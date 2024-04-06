4e5e5959a8636f0120840c560b706bdb25ba4521
ningpop <craft1933@naver.com>
2024-04-06 13:43:28 +0900

    init : project setting

---
+++ src/main/java/com/callmepeace/lockorrock/LockorrockApplication.java
@@ -0,0 +1,13 @@
+package com.callmepeace.lockorrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockorrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockorrockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callmepeace/lockorrock/LockorrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callmepeace.lockorrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockorrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
